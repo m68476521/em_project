fdf9a23eb93e8166a7b847d88b6cfbe906b284fc
Mike <m68476521.1@gmail.com>
2019-05-29 23:06:40 -0700

    Add image poster
    by m68476521.1@gmail.com

--- app/src/main/java/com/m68476521/mike/em_project/MainMenuHolder.kt
+++ app/src/main/java/com/m68476521/mike/em_project/MainMenuHolder.kt
@@ -3,8 +3,16 @@ package com.m68476521.mike.em_project
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.m68476521.mike.em.Results
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_main_menu.view.*
 
 class MainMenuHolder(view: View) : RecyclerView.ViewHolder(view) {
-    fun bindResult(result: Results) = itemView.name.text = result.name
+    fun bindResult(result: Results) = with(itemView) {
+        itemView.name.text = result.name
+        var imageSource = "${result.thumbnail.path}.${result.thumbnail.extension}"
+        imageSource = imageSource.replace("http", "https")
+        Picasso.get()
+            .load(imageSource)
+            .into(itemView.imagePoster)
+    }
 }
