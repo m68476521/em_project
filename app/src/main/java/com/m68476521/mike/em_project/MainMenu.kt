663f151160d410deb242ddb6481c4b185220dfca
Mike <m68476521.1@gmail.com>
2019-07-07 17:21:22 -0700

    Change the start destination fragment
    - Add star fragment that will be the inital fragment
    - Add lottie animation
    - Move fetch data to star fragment
    
    by m68476521.com

--- app/src/main/java/com/m68476521/mike/em_project/MainMenu.kt
+++ app/src/main/java/com/m68476521/mike/em_project/MainMenu.kt
@@ -10,11 +10,8 @@ import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.m68476521.mike.em_project.mainMenu.MainMenuAdapter
-import com.m68476521.mike.em_project.utils.ApiManager
 import com.m68476521.mike.em_project.utils.DataModel
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_main_menu.*
 
 /**
@@ -22,9 +19,7 @@ import kotlinx.android.synthetic.main.fragment_main_menu.*
  * by m68476521.com
  */
 class MainMenu : Fragment() {
-    private val compositeDisposable = CompositeDisposable()
     private val adapter = MainMenuAdapter()
-    private lateinit var model: DataModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -38,24 +33,7 @@ class MainMenu : Fragment() {
         recyclerMenu.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
         recyclerMenu.itemAnimator = DefaultItemAnimator()
         recyclerMenu.adapter = adapter
-        getData()
-    }
-
-    private fun getData() {
-        ApiManager.setUp()
-        val eventsDisposable = ApiManager.api.getPublicCharacters(10)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
-            .subscribe({
-                model = ViewModelProviders.of(requireActivity()).get(DataModel::class.java)
-                model.results = it.data.results
-                adapter.swapResults(it.data.results)
-            }, { it.printStackTrace() })
-        compositeDisposable.add(eventsDisposable)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
+        val model = ViewModelProviders.of(requireActivity()).get(DataModel::class.java)
+        adapter.swapResults(model.results)
     }
 }
---
+++ app/src/main/java/com/m68476521/mike/em_project/StarFragment.kt
@@ -0,0 +1,55 @@
+package com.m68476521.mike.em_project
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
+import com.m68476521.mike.em_project.utils.ApiManager
+import com.m68476521.mike.em_project.utils.DataModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * by m68476521.com
+ *
+ */
+class StarFragment : Fragment() {
+    private val compositeDisposable = CompositeDisposable()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_star, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fetchData()
+    }
+
+    private fun fetchData() {
+        ApiManager.setUp()
+        val eventsDisposable = ApiManager.api.getPublicCharacters(10)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                val model = ViewModelProviders.of(requireActivity()).get(DataModel::class.java)
+                model.results = it.data.results
+                view?.findNavController()?.navigate(StarFragmentDirections.actionStarFragmentToMainMenu())
+            }, {
+                it.printStackTrace()
+            })
+        compositeDisposable.add(eventsDisposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
+    }
+}
